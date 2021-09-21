dd691ca0567b086121d33ff03029e5c55bf3bf8b
João Severo <joaovbsevero@gmail.com>
2021-09-21 18:22:29 -0300

    .

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const router = require('./src/routes');
 
 app.use(router);
 
-app.get('/', (_, res) => res.send({ status: 'Status: Ok' }));
+app.get('/', (_, res) => res.send({ status: 'Ok' }));
 
 app.listen(config.apiPort, () => {
-  console.log(
-    `API Server -${config.apiName}- started and listening on http://${config.apiURL}:${config.apiPort}/ (${config.env})`
-  );
+    console.log(
+        `API Server -${config.apiName}- started and listening on http://${config.apiURL}:${config.apiPort}/ (${config.env})`
+    );
 });
 
 module.exports = app;
